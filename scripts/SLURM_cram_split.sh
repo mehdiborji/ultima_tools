5171a0bdd79b82e66c8324ad9c7a340ab36883e3
mehdiborji <borjijiji@gmail.com>
2024-04-30 15:53:56 -0400

    minor update to reduce res > only few cores seem to be used, need to check if pipeing samtools is reducing efficiency or it is just IO limit

--- scripts/SLURM_cram_split.sh
+++ scripts/SLURM_cram_split.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -c 20
-#SBATCH --mem=10G
-#SBATCH -t 0:50:00
+#SBATCH -c 10
+#SBATCH --mem=1G
+#SBATCH -t 0:30:00
 #SBATCH -p short
 #SBATCH --account=chen_fec176
 #SBATCH -o ultima_cram_split_fq_%A.out
@@ -9,4 +9,4 @@
 echo 'indir =' $1
 echo 'sample =' $2
 
-python ~/ultima_demux/cram_pipeline.py -c 20 -i $1 -s $2
+python ~/ultima_demux/cram_pipeline.py -c 10 -i $1 -s $2

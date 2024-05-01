f98d88c3a1e24db71b68b3bb4f680f3fcf510686
mehdiborji <borjijiji@gmail.com>
2024-05-01 15:52:46 -0400

    update run resources

--- scripts/SLURM_cram_split.sh
+++ scripts/SLURM_cram_split.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -c 10
-#SBATCH --mem=1G
+#SBATCH -c 20
+#SBATCH --mem=2G
 #SBATCH -t 0:30:00
 #SBATCH -p short
 #SBATCH --account=chen_fec176
@@ -9,4 +9,5 @@
 echo 'indir =' $1
 echo 'sample =' $2
 
-python ~/ultima_demux/cram_pipeline.py -c 10 -i $1 -s $2
+python ~/ultima_demux/cram_pipeline.py -c 20 -i $1 -s $2
+#--limit
